9643a9d4f00b9086b6f04900c20ba3dde44b12c7
gbzin <95337563+kznybs@users.noreply.github.com>
2024-05-04 17:39:09 -0400

    Create config.php

---
+++ formulario/config.php
@@ -0,0 +1,19 @@
+<?php
+
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'formulario';
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+    // if($conexao->connect_errno)
+    // {
+    //     echo "Erro";
+    // }
+    // else
+    // {
+    //     echo "Conexão efetuada com sucesso";
+    // }
+
+?>
