c5213466c1b459f4719732e9b52cb86a4e73be88
gbzin <95337563+kznybs@users.noreply.github.com>
2024-05-04 19:25:51 -0400

    Create edit.php

---
+++ usuario/edit.php
@@ -0,0 +1,182 @@
+<?php
+    include_once('config.php');
+    include('protecct.php');
+    if(!empty($_GET['id']))
+    {
+        $id = $_GET['id'];
+        $sqlSelect = "SELECT * FROM usuario WHERE id=$id";
+        $result = $conexao->query($sqlSelect);
+        if($result->num_rows > 0)
+        {
+            while($user_data = mysqli_fetch_assoc($result))
+            {
+                $username = $user_data['username'];
+                $cargo = $user_data['cargo'];
+                $email = $user_data['email'];
+                $telefone = $user_data['telefone'];
+                $sexo = $user_data['sexo'];
+                $data_nasc = $user_data['data_nasc'];
+                $banco = $user_data['banco'];
+                $chave = $user_data['chave'];
+                $endereco = $user_data['endereco'];
+                $password = $user_data['password'];
+                $created_at = $user_data['created_at'];
+            }
+        }
+        else
+        {
+            header('Location: view.php');
+        }
+    }
+    else
+    {
+        header('Location: view.php');
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar Dados</title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+            background-image: linear-gradient(to right, #f0f0f0, #f0f0f0);
+        }
+        .box{
+            color: white;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            background-color: rgba(0, 0, 0, 0.6);
+            padding: 15px;
+            border-radius: 15px;
+            width: 20%;
+        }
+        fieldset{
+            border: 3px solid rgb(197, 94, 10);
+        }
+        legend{
+            border: 1px solid rgb(197, 94, 10);
+            padding: 10px;
+            text-align: center;
+            background-color: rgb(197, 94, 10);
+            border-radius: 8px;
+        }
+        .inputBox{
+            position: relative;
+        }
+        .inputUser{
+            background: none;
+            border: none;
+            border-bottom: 1px solid white;
+            outline: none;
+            color: white;
+            font-size: 15px;
+            width: 100%;
+            letter-spacing: 2px;
+        }
+        .labelInput{
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            pointer-events: none;
+            transition: .5s;
+        }
+        .inputUser:focus ~ .labelInput,
+        .inputUser:valid ~ .labelInput{
+            top: -20px;
+            font-size: 12px;
+            color: rgb(255, 255, 255);
+        }
+        #data_nascimento{
+            border: none;
+            padding: 8px;
+            border-radius: 10px;
+            outline: none;
+            font-size: 15px;
+        }
+        #submit{
+            background-image: linear-gradient(to right,rgb(197, 94, 10), rgb(197, 94, 10));
+            width: 100%;
+            border: none;
+            padding: 15px;
+            color: rgb(255, 255, 255);
+            font-size: 15px;
+            cursor: pointer;
+            border-radius: 10px;
+        }
+        #submit:hover{
+            background-image: linear-gradient(to right,#009320, #009320);
+        }
+    </style>
+</head>
+<body>
+    <a href="view.php">Voltar</a>
+    <div class="box">
+        <form action="saveEdit.php" method="POST">
+            <fieldset>
+                <legend><b>Editar Cliente</b></legend>
+                <br>
+                <div class="inputBox">
+                    <input type="text" name="username" id="username" class="inputUser" value=<?php echo $username;?> required>
+                    <label for="username" class="labelInput">Nome completo</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="cargo" id="cargo" class="inputUser" value=<?php echo $cargo;?> required>
+                    <label for="cargo" class="labelInput">Cargo</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="email" id="email" class="inputUser" value=<?php echo $email;?> required>
+                    <label for="email" class="labelInput">Email</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="tel" name="telefone" id="telefone" class="inputUser" value=<?php echo $telefone;?> required>
+                    <label for="telefone" class="labelInput">Telefone</label>
+                </div>
+                <p>Sexo:</p>
+                <input type="radio" id="feminino" name="genero" value="feminino" <?php echo ($sexo == 'feminino') ? 'checked' : '';?> required>
+                <label for="feminino">Feminino</label>
+                <br>
+                <input type="radio" id="masculino" name="genero" value="masculino" <?php echo ($sexo == 'masculino') ? 'checked' : '';?> required>
+                <label for="masculino">Masculino</label>
+                <br>
+                <input type="radio" id="outro" name="genero" value="outro" <?php echo ($sexo == 'outro') ? 'checked' : '';?> required>
+                <label for="outro">Outro</label>
+                <br><br>
+                <label for="data_nascimento"><b>Data de Nascimento:</b></label>
+                <input type="date" name="data_nascimento" id="data_nascimento" value=<?php echo $data_nasc;?> required>
+                <br><br><br>
+                <div class="inputBox">
+                    <input type="text" name="banco" id="banco" class="inputUser" value=<?php echo $banco;?> required>
+                    <label for="banco" class="labelInput">Banco</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="chave" id="chave" class="inputUser" value=<?php echo $chave;?> required>
+                    <label for="chave" class="labelInput">Chave Pix</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="endereco" id="endereco" class="inputUser" value=<?php echo $endereco;?> required>
+                    <label for="endereco" class="labelInput">Endereço</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="password" id="password" class="inputUser" required>
+                    <label for="password" class="labelInput">Senha Para Login</label>
+                </div>
+                <br><br>
+				<input type="hidden" name="id" value=<?php echo $id;?>>
+                <input type="submit" name="update" id="submit">
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
